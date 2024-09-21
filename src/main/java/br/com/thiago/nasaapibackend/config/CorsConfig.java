df4b9c1fdeed7d7a5e09b9f078abc8ff2a122bb1
thiagomag <thiagomagdalena@gmail.com>
2024-09-21 16:59:07 -0300

    adding cors config

---
+++ src/main/java/br/com/thiago/nasaapibackend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package br.com.thiago.nasaapibackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")// URL do seu frontend
+                        .allowedOrigins("http://ec2-34-228-189-95.compute-1.amazonaws.com:5173")  // URL do seu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
